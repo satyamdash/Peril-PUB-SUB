88d329d1e8be4318ed6c1f349fa8cf89f324406f
satyamdash <satyam.bn6@gmail.com>
2025-11-16 08:39:34 +0530

    Init Repo

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+	conn_string := "amqp://guest:guest@localhost:5672/"
+
+	conn, err := amqp.Dial(conn_string)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	// amqp_chan, err := conn.Channel()
+	defer conn.Close()
+	fmt.Println("Connection was successful")
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	<-signalChan
 }
---
+++ internal/pubsub/pubsub.go
@@ -0,0 +1,5 @@
+package pubsub
+
+// func PublishJSON[T any](ch *amqp.Channel, exchange, key string, val T) error {
+
+// }
